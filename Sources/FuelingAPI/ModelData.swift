9896a19b1f27f685e16000f393d9af89956cf96a
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-18 10:28:11 -0400

    Update ModelData mapping for renamed Location entity

--- Sources/FuelingAPI/ModelData.swift
+++ Sources/FuelingAPI/ModelData.swift
@@ -11,64 +11,64 @@ import Foundation
 import CoreModel
 import CoreFueling
 
-// MARK: - Location
+// MARK: - GetLocation
 
-public extension Location {
+public extension GetLocation {
 
-    /// Parsed site identifier.
-    var id: Site.ID? {
-        Site.ID.Prefixed(rawValue: siteID).map { Site.ID($0) }
+    /// Parsed location identifier.
+    var id: Location.ID? {
+        Location.ID.Prefixed(rawValue: siteID).map { Location.ID($0) }
     }
 }
 
 public extension ModelData {
 
     /// Convert a location response into the entity graph to persist
-    /// (site + fuel options).
+    /// (location + fuel options).
     static func location(
-        _ location: Location,
+        _ location: GetLocation,
         lastCached: Date = Date()
     ) -> [ModelData] {
-        guard let siteData = ModelData(location: location, lastCached: lastCached) else {
+        guard let locationData = ModelData(location: location, lastCached: lastCached) else {
             return []
         }
-        var modelData = [siteData]
+        var modelData = [locationData]
         modelData += (location.fuelingOptions ?? [])
             .compactMap { ModelData(fuelOption: $0) }
         return modelData
     }
 
-    /// Site entity data for a location response.
+    /// Location entity data for a location response.
     init?(
-        location: Location,
+        location: GetLocation,
         lastCached: Date = Date()
     ) {
         guard let id = location.id else {
             return nil
         }
-        self.init(entity: Site.entityName, id: ObjectID(id))
+        self.init(entity: Location.entityName, id: ObjectID(id))
 
         // set attributes
-        attributes[.init(Site.CodingKeys.name)] = .string(location.name)
-        attributes[.init(Site.CodingKeys.brand)] = location.storeBrand.map { .string($0) } ?? .null
-        attributes[.init(Site.CodingKeys.address)] = .string(location.address)
-        attributes[.init(Site.CodingKeys.city)] = .string(location.city)
-        attributes[.init(Site.CodingKeys.state)] = .string(location.state)
-        attributes[.init(Site.CodingKeys.zipCode)] = .string(location.zipCode)
-        attributes[.init(Site.CodingKeys.phone)] = .string(location.phoneNumbers.primaryPhoneNumber ?? "")
-        attributes[.init(Site.CodingKeys.directions)] = location.directions.map { .string($0) } ?? .null
-        attributes[.init(Site.CodingKeys.latitude)] = .double(location.latitude)
-        attributes[.init(Site.CodingKeys.longitude)] = .double(location.longitude)
-        attributes[.init(Site.CodingKeys.fuelLanes)] = .int16(location.dieselDispenserLanes.map { numericCast($0) } ?? 0)
-        attributes[.init(Site.CodingKeys.truckParkingSpaces)] = .int16(location.truckParkingSpaces.map { numericCast($0) } ?? 0)
-        attributes[.init(Site.CodingKeys.showers)] = .int16(location.privateShowers.map { numericCast($0) } ?? 0)
-        attributes[.init(Site.CodingKeys.lastCached)] = .date(lastCached)
+        attributes[.init(Location.CodingKeys.name)] = .string(location.name)
+        attributes[.init(Location.CodingKeys.brand)] = location.storeBrand.map { .string($0) } ?? .null
+        attributes[.init(Location.CodingKeys.address)] = .string(location.address)
+        attributes[.init(Location.CodingKeys.city)] = .string(location.city)
+        attributes[.init(Location.CodingKeys.state)] = .string(location.state)
+        attributes[.init(Location.CodingKeys.zipCode)] = .string(location.zipCode)
+        attributes[.init(Location.CodingKeys.phone)] = .string(location.phoneNumbers.primaryPhoneNumber ?? "")
+        attributes[.init(Location.CodingKeys.directions)] = location.directions.map { .string($0) } ?? .null
+        attributes[.init(Location.CodingKeys.latitude)] = .double(location.latitude)
+        attributes[.init(Location.CodingKeys.longitude)] = .double(location.longitude)
+        attributes[.init(Location.CodingKeys.fuelLanes)] = .int16(location.dieselDispenserLanes.map { numericCast($0) } ?? 0)
+        attributes[.init(Location.CodingKeys.truckParkingSpaces)] = .int16(location.truckParkingSpaces.map { numericCast($0) } ?? 0)
+        attributes[.init(Location.CodingKeys.showers)] = .int16(location.privateShowers.map { numericCast($0) } ?? 0)
+        attributes[.init(Location.CodingKeys.lastCached)] = .date(lastCached)
 
         // set relationships
         let fuelOptions = (location.fuelingOptions ?? [])
             .compactMap { FuelOption.ID(name: $0) }
             .map { ObjectID($0) }
-        relationships[.init(Site.CodingKeys.fuelOptions)] = .toMany(fuelOptions)
+        relationships[.init(Location.CodingKeys.fuelOptions)] = .toMany(fuelOptions)
     }
 
     /// Fuel option entity data for a fueling option name.
@@ -85,14 +85,14 @@ public extension ModelData {
 
 public extension FuelPrice {
 
-    /// Parsed site identifier.
-    var site: Site.ID? {
-        Site.ID.Prefixed(rawValue: siteID).map { Site.ID($0) }
+    /// Parsed location identifier.
+    var location: Location.ID? {
+        Location.ID.Prefixed(rawValue: siteID).map { Location.ID($0) }
     }
 
     /// Fuel product identifier for this price entry.
     var product: FuelProduct.ID? {
-        site.map { .fuelPrice(fuelCode, site: $0) }
+        location.map { .fuelPrice(fuelCode, location: $0) }
     }
 }
 
@@ -103,7 +103,7 @@ public extension ModelData {
         fuelPrice: FuelPrice,
         lastCached: Date = Date()
     ) {
-        guard let site = fuelPrice.site,
+        guard let location = fuelPrice.location,
             let product = fuelPrice.product
         else {
             return nil
@@ -113,6 +113,6 @@ public extension ModelData {
         attributes[.init(FuelProduct.CodingKeys.price)] = .double(fuelPrice.price)
         attributes[.init(FuelProduct.CodingKeys.descriptionText)] = .string(fuelPrice.productDescription)
         attributes[.init(FuelProduct.CodingKeys.lastCached)] = .date(lastCached)
-        relationships[.init(FuelProduct.CodingKeys.site)] = .toOne(ObjectID(site))
+        relationships[.init(FuelProduct.CodingKeys.location)] = .toOne(ObjectID(location))
     }
 }
